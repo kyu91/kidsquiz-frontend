7df209671aa63c0e0d3d3b349af2f87a5eac5169
SeokKyuHong <hongsk5689@gmail.com>
2023-01-21 17:06:28 +0900

    [석규]폰트 우선 삭제

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import '@fontsource/public-sans';
+// import '@fontsource/public-sans';
 import { BrowserRouter } from 'react-router-dom';
 
 
